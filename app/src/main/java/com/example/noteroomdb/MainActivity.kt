623e4d73e3542d139418704f2c0318a3e2f35c6a
Oyewale Abdulsobur <66224527+smilingprogrammer@users.noreply.github.com>
2022-06-27 11:51:33 +0100

    Built NoteAdapter

--- app/src/main/java/com/example/noteroomdb/MainActivity.kt
+++ app/src/main/java/com/example/noteroomdb/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.noteroomdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var noteAdapter: NoteAdapter
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var addNoteBtn: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private fun init(){
+        noteAdapter = NoteAdapter()
+        linearLayoutManager = LinearLayoutManager(this)
+        recyclerView = findViewById(R.id.recyclerview)
+        addNoteBtn = findViewById(R.id.float_button)
+        recyclerView.adapter = noteAdapter
+        recyclerView.layoutManager = linearLayoutManager
     }
 }
---
+++ app/src/main/java/com/example/noteroomdb/NoteAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.noteroomdb
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.noteroomdb.db.entities.NoteEntity
+
+class NoteAdapter: ListAdapter<NoteEntity, NoteAdapter.NoteViewHolder>(NoteDiffUtil()){
+
+    private class NoteDiffUtil: DiffUtil.ItemCallback<NoteEntity>(){
+        override fun areItemsTheSame(oldItem: NoteEntity, newItem: NoteEntity): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: NoteEntity, newItem: NoteEntity): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+    }
+
+    inner class NoteViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        fun bind(item: NoteEntity){
+            val noteTitle = itemView.findViewById<TextView>(R.id.note_title)
+            val noteDescription = itemView.findViewById<TextView>(R.id.note_description)
+
+            noteTitle.text = item.title
+            noteDescription.text = item.description
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.item_note, parent, false)
+        return NoteViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
+}
